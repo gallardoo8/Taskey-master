8b684695bc2ea899b3a21401513e0a9b818fc944
Bernardo Nolasco <rogue010412@outlook.com>
2026-03-16 12:09:44 -0600

    Implement initial user type detection and redirection based on stored tokens.

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,51 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Redirect } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 
+//verifica que el usuario tenga un token guardadp en cache y redirige a la pantalla correspondiente padre o hijo
 export default function Index() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [userType, setUserType] = useState('none');
+
+    useEffect(() => {
+        const checkToken = async () => {
+            try {
+                const parentToken = await AsyncStorage.getItem('parent_token');
+                if (parentToken) {
+                    setUserType('parent');
+                    return;
+                }
+
+                const childToken = await AsyncStorage.getItem('child_token');
+                if (childToken) {
+                    setUserType('child');
+                    return;
+                }
+            } catch (error) {
+                console.log('Error checking token:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        checkToken();
+    }, []);
+
+    if (isLoading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#7E22CE' }}>
+                <ActivityIndicator size="large" color="white" />
+            </View>
+        );
+    }
+
+    if (userType === 'parent') {
+        return <Redirect href="/principalpapa" />;
+    }
+
+    if (userType === 'child') {
+        return <Redirect href="/perfilhijo" />;
+    }
+
     return <Redirect href="/PrimeraPantalla" />;
 }
